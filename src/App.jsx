17a9d9f9ca5ead762d2156534f0bf2abcc25758a
mohammed shadhir <mohammedshadhir5@gmail.com>
2025-08-03 13:00:10 +0530

    add assets and gsap navtween add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import NavBar from "./components/NavBar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 class="text-3xl font-bold underline text-amber-300">Hello world!</h1>
-    </div>
+    <main>
+      <NavBar />
+    </main>
   );
 };
 
